5ede02163a8ab951f3b97706b52d332154df66a1
yama-dev <tatsuya.yamamoto69@gmail.com>
2019-12-12 19:38:09 +0900

    update version text.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 /*!
  * DEV TEMPLATE GULP
- * Version 0.9.2
+ * Version 0.10.0
  * Repository https://github.com/yama-dev/dev-template-gulp
  * Copyright yama-dev
  * Licensed under the MIT license.
