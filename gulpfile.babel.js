b827e8998cfecafa2dff2ee6e6569a52e6b547ab
yama-dev <tatsuya.yamamoto69@gmail.com>
2020-01-07 09:30:11 +0900

    adjust config path.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -13,11 +13,11 @@ let _copy = `${pkg.name.toUpperCase()}
 - Licensed under the ${pkg.license} license.`;
 console.log('-'.repeat(38) + '\n'+ _copy + '\n'+'-'.repeat(38));
 import { series, parallel } from 'gulp';
-import taskSass from './dev/sass';
-import taskJsBabel from './dev/javascript_babel';
-import taskTemplate from './dev/template';
-import taskServer from './dev/server';
-import taskWatch from './dev/watch';
+import taskSass from './.dev/sass';
+import taskJsBabel from './.dev/javascript_babel';
+import taskTemplate from './.dev/template';
+import taskServer from './.dev/server';
+import taskWatch from './.dev/watch';
 
 export default series(
   taskTemplate,
