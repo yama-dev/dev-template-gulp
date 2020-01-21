6769bf5f43e53ffed2997317652e252e5553bd88
yama-dev <tatsuya.yamamoto69@gmail.com>
2020-01-21 10:05:42 +0900

    adjsut .dev\config\env.js

--- .dev/config/env.js
+++ .dev/config/env.js
@@ -10,6 +10,7 @@ ARGV.map((item,i)=>{
     if(ARGV[i+1]){
       if(!/--/.test(ARGV[i+1])) _env[_item] = ARGV[i+1];
       else _env[_item] = true;
+      _env[_item] = _env[_item] === 'false' ? false : _env[_item];
     } else {
       _env[_item] = true;
     }
