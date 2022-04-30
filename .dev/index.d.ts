9a963b832ef86700974930ad786222fd2e97b22a
yama-dev <tatsuya.yamamoto69@gmail.com>
2022-04-30 10:31:55 +0900

    add ts config

---
+++ .dev/index.d.ts
@@ -0,0 +1,37 @@
+
+declare module 'node-notifier'
+
+declare module 'gulp';
+declare module 'gulp-sass';
+declare module 'gulp-postcss';
+declare module 'gulp-cached';
+declare module 'gulp-plumber';
+declare module 'gulp-ignore';
+declare module 'gulp-prompt';
+declare module 'gulp-htmlmin';
+declare module 'gulp-htmlhint';
+declare module 'gulp-imagemin';
+declare module 'gulp-if';
+declare module 'gulp-javascript-obfuscator';
+declare module 'gulp-terser';
+declare module 'gulp-eslint';
+declare module 'gulp-prettier';
+declare module 'gulp-babel';
+declare module 'gulp-uglify';
+declare module 'gulp-rename';
+declare module 'gulp-ejs';
+
+declare module '@yama-dev/gulp-stream-util';
+declare module '@yama-dev/data-collector';
+declare module '@yama-dev/data-template';
+
+declare module 'webpack'
+declare module 'webpack-stream';
+
+declare module 'node-sass';
+declare module 'pixrem';
+declare module 'autoprefixer';
+declare module 'cssnano';
+declare module 'css-declaration-sorter';
+declare module 'postcss-combine-media-query';
+
